b2279f2dc3c8addf06bed3fc016550696d11d7cf
Luis Gerardo Castañeda López <55543994+REMKA-HAZE@users.noreply.github.com>
2020-03-13 08:26:05 -0600

    se agrego el modelo y la ruta de customers

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
 app.use(require('./User'));
-=======
 
-app.use(require('./house'));
+app.use('/customer',require('./customer'));
 
->>>>>>> b0df2d52672fae4f1deac4d896d4da4cac94f219
 
 module.exports = app;
